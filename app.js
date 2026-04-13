807ed9b2c478aa0a43da2d3a05699a60a07ce13b
Harsh Kumar <harshkr70@gmail.com>
2026-04-13 07:21:19 +0530

    fix: use dynamic PORT for Render deployment and correct error handling

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const { indexRouter } = require("./router/indexRouter.js");
 const { messageRouter } = require("./router/messageRouter.js");
 
-const PORT = 3030;
+const PORT = process.env.PORT || 3030;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
